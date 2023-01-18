18ac84fe834ced3bc88fe6c893dbe51d8fe6b52f
Gaurav singh <gauravaswal60@gmail.com>
2023-01-18 21:13:07 +0530

    worked ond

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Main from "../src/pages/main"
+import Login from "../src/pages/signin"
 const App = ()=>{
 return (
     <BrowserRouter>
        <Routes>
          <Route path="/" element={<Main />} />
+         <Route path="/login" element={<Login />} />
+
        </Routes>
     </BrowserRouter>
 )
--- src/pages/google-login.js
+++ src/pages/google-login.js
@@ -1,49 +1,49 @@
-import React, { useState } from 'react';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
+// import React, { useState } from 'react';
+// import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
-const clientId = "811713109418-hjlfkech9s0ittgv71m6afit58s30md0.apps.googleusercontent.com";
+// const clientId = "";
 
-const Google = () => {
-    const [showloginButton, setShowloginButton] = useState(true);
-    const [showlogoutButton, setShowlogoutButton] = useState(false);
-    const onLoginSuccess = (res) => {
-        console.log('Login Success:', res.profileObj);
-        setShowloginButton(false);
-        setShowlogoutButton(true);
-    };
+// const Google = () => {
+//     const [showloginButton, setShowloginButton] = useState(true);
+//     const [showlogoutButton, setShowlogoutButton] = useState(false);
+//     const onLoginSuccess = (res) => {
+//         console.log('Login Success:', res.profileObj);
+//         setShowloginButton(false);
+//         setShowlogoutButton(true);
+//     };
 
-    const onLoginFailure = (res) => {
-        console.log('Login Failed:', res);
-    };
+//     const onLoginFailure = (res) => {
+//         console.log('Login Failed:', res);
+//     };
 
-    const onSignoutSuccess = () => {
-        alert("You have been logged out successfully");
-        console.clear();
-        setShowloginButton(true);
-        setShowlogoutButton(false);
-    };
+//     const onSignoutSuccess = () => {
+//         alert("You have been logged out successfully");
+//         console.clear();
+//         setShowloginButton(true);
+//         setShowlogoutButton(false);
+//     };
 
-    return (
-        <div>
-            {showloginButton ?
-                <GoogleLogin
-                    clientId={clientId}
-                    buttonText="Sign In"
-                    onSuccess={onLoginSuccess}
-                    onFailure={onLoginFailure}
-                    cookiePolicy={'single_host_origin'}
-                    isSignedIn={true}
-                /> : null}
+//     return (
+//         <div>
+//             {showloginButton ?
+//                 <GoogleLogin
+//                     clientId={clientId}
+//                     buttonText="Sign In"
+//                     onSuccess={onLoginSuccess}
+//                     onFailure={onLoginFailure}
+//                     cookiePolicy={'single_host_origin'}
+//                     isSignedIn={true}
+//                 /> : null}
 
-            {showlogoutButton ?
-                <GoogleLogout
-                    clientId={clientId}
-                    buttonText="Sign Out"
-                    onLogoutSuccess={onSignoutSuccess}
-                >
-                </GoogleLogout> : null
-            }
-        </div>
-    );
-}
-export default Google;
+//             {showlogoutButton ?
+//                 <GoogleLogout
+//                     clientId={clientId}
+//                     buttonText="Sign Out"
+//                     onLogoutSuccess={onSignoutSuccess}
+//                 >
+//                 </GoogleLogout> : null
+//             }
+//         </div>
+//     );
+// }
+// export default Google;
